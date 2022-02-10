f97d060757f4e1590511d123ba63bb478b45afcf
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-02-09 20:19:59 -0700

    Add annotations line 2-5

--- challenge/harryPotter.js
+++ challenge/harryPotter.js
@@ -1,8 +1,8 @@
 //This file is named harryPotter.js because it is relevant to books
-var isWizard = true
-var isMuggle = true
-var numOfBooks = 9
-var mainChar = 3
+var isWizard = true //Harry is wizard
+var isMuggle = true //Harry is also muggle
+var numOfBooks = 9 //There are nine books, I think. I'm a movie guy.
+var mainChar = 3 //3 main characters in the series.
 var potter = "Harry Potter"
 var villian = "Voldemort"
 var otherChar = ["Hermoine", "Ron", "Dumbledore", "Snape"]
